c6b2a784d1136753f18c189ea447365dd08b0ce0
as27 <andreas@apxp.info>
2016-08-12 11:32:37 +0200

    Client html

--- html/app.js
+++ html/app.js
@@ -3,19 +3,23 @@ var serverURL = "http://raspi.fritz.box:4558/folder/2016"
 new Vue({
   el: '#app',
   data: {
-    path: "/2016-08",
-    folder: null
+    path: "",
+    content: null
   },
   created: function () {
     this.fetchData()
   },
   methods: {
+    openFolder: function(f){
+      this.path = this.path + "/" + f
+      this.fetchData()
+    },
     fetchData: function () {
       var xhr = new XMLHttpRequest()
       var self = this
       xhr.open('GET', serverURL+self.path)
       xhr.onload = function () {
-        self.folder = JSON.parse(xhr.responseText)
+        self.content = JSON.parse(xhr.responseText)
       }
       xhr.send()
     }
